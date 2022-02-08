da528d90485747da7f38587f9670f9c29283dfcc
Alex Teerlinck <shadows32426@gmail.com>
2022-02-07 23:27:16 -0600

    Finished

---
+++ src/main/java/com/example/springbootassignment/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.springbootassignment;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
+    @RequestMapping("/step1")
+    public String showStep1() { return "pages/Step1";}
+    @RequestMapping("/step2")
+    public String showStep2() { return "pages/Step2";}
+
+    @RequestMapping("/step3")
+    public String showStep3() { return "pages/Step3";}
+}
